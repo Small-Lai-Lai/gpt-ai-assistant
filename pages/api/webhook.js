c0371c66587f0834dfc1ab73c544207bd5cd0880
Small-Lai-Lai <vul3x94x94932@yahoo.com.tw>
2025-04-24 01:36:19 +0800

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,1 +1,10 @@
 
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    // 可以加上你自己的驗證與處理邏輯
+    console.log('LINE Webhook received:', req.body);
+    return res.status(200).send('OK');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
